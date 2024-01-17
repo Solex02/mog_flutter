5dca1fddccae8968327eca0084ee6a7fd055cac5
MarioDemon <146721976+MarioDemon@users.noreply.github.com>
2024-01-17 13:06:30 +0100

    Pantalla Principal con la base hecha

--- lib/pages/Principal.dart
+++ lib/pages/Principal.dart
@@ -1,17 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MainActivity(),
-    );
-  }
-}
 
 class MainActivity extends StatefulWidget {
   @override
