60f12c2fec515b5d356f28d198b55d0ea0c269e2
MarioDemon <146721976+MarioDemon@users.noreply.github.com>
2024-02-27 11:20:53 +0100

    Conexion base de datos publicaciones

--- lib/pages/Principal.dart
+++ lib/pages/Principal.dart
@@ -1,12 +1,15 @@
 import 'dart:convert';
 import 'dart:io';
-
-import 'package:flutter/services.dart';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
-import 'package:mog_flutter/pages/profile.dart';
-import 'package:mog_flutter/pages/ranking.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'upload_content.dart'; // Importa el nuevo archivo
+import 'package:supabase/supabase.dart';
+import 'package:uuid/uuid.dart';
+
+final supabase = SupabaseClient(
+  'https://ngejlljkgxzpnwznpddk.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5nZWpsbGprZ3h6cG53em5wZGRrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY0MDY1NjMsImV4cCI6MjAxMTk4MjU2M30.nlZnIiHCjiThvu-cLj_aBPYaGE1knPFWXOhCkJQDLL4',
+);
 
 class MainActivity extends StatefulWidget {
   @override
@@ -20,50 +23,46 @@ class _MainActivityState extends State<MainActivity> {
   @override
   void initState() {
     super.initState();
-    // Carga las publicaciones guardadas al iniciar la aplicación
-    loadPublications();
+    _loadPublications(); // Carga las publicaciones al inicio
   }
 
-  // Guarda las publicaciones en SharedPreferences
-  Future<void> savePublications() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    List<String> serializedPublications = _publications
-        .map((widget) => jsonEncode((widget as ImageItem).toJson()))
-        .toList();
-    prefs.setStringList('publications', serializedPublications);
+  void handleUpload(String imagePath, String description) async {
+    List<int> bytes = await File(imagePath).readAsBytes();
+    String imageData = base64Encode(bytes);
+    final response = await supabase
+        .from('Publicacionpureba')
+        .insert({'image_data': imageData, 'description': description})
+        .execute();
+
+    _loadPublications(); // Recarga las publicaciones después de insertar
+    setState(() {
+      _currentIndex = 0; // Cambia a la pantalla principal después de cargar
+    });
   }
 
-  // Carga las publicaciones desde SharedPreferences
-  Future<void> loadPublications() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    List<String>? serializedPublications = prefs.getStringList('publications');
+  void _loadPublications() async {
+    final response = await supabase.from('Publicacionpureba').select().execute();
 
-    if (serializedPublications != null) {
-      List<Widget> loadedPublications = serializedPublications
-          .map((json) => ImageItem.fromJson(jsonDecode(json)))
-          .toList();
+    if (response != null && response.data != null) {
+      List<Map<String, dynamic>> publications =
+          (response.data as List<dynamic>).cast<Map<String, dynamic>>();
 
       setState(() {
-        _publications = loadedPublications;
+        _publications = publications.map((publication) {
+          String imageData = publication['image_data'];
+          String description = publication['description'];
+
+          return ImageItem(
+            imageData: imageData,
+            description: description,
+            logoImagePath: 'assets/images/oscar.png',
+            logoText: 'Gangzalo',
+          );
+        }).toList();
       });
+    } else {
+      print('Error fetching publications');
     }
-  }
-
-  void handleUpload(String imagePath, String description) {
-    setState(() {
-      _publications.add(
-        ImageItem(
-          imagePath: imagePath,
-          description: description,
-          logoImagePath: 'assets/images/oscar.png',
-          logoText: 'Gangzalo',
-        ),
-      );
-      _currentIndex = 0;
-    });
-
-    // Guarda las publicaciones después de agregar una nueva
-    savePublications();
   }
 
   @override
@@ -100,25 +99,13 @@ class _MainActivityState extends State<MainActivity> {
                   itemCount: _publications.length,
                   itemBuilder: (context, index) => _publications[index],
                 ),
-
                 Container(), // Contenedor vacío para la pantalla de carga
               ],
             ),
           ),
-          if (_currentIndex ==
-              1) // Muestra la pantalla de carga solo cuando se selecciona "Upload"
-            Positioned.fill(
-              child: RankingPage(),
-            ),
-          if (_currentIndex ==
-              2) // Muestra la pantalla de carga solo cuando se selecciona "Upload"
+          if (_currentIndex == 2) // Muestra la pantalla de carga solo cuando se selecciona "Upload"
             Positioned.fill(
               child: UploadContent(onUpload: handleUpload),
-            ),
-          if (_currentIndex ==
-              3) // Muestra la pantalla de carga solo cuando se selecciona "Upload"
-            Positioned.fill(
-              child: ProfilePage(),
             ),
         ],
       ),
@@ -154,35 +141,19 @@ class _MainActivityState extends State<MainActivity> {
   }
 }
 
-//LISTA DE PUBLICACIONES
-
+// Widget para mostrar una publicación individual
 class ImageItem extends StatefulWidget {
-  final String imagePath;
+  final String imageData; // Cambiado a String para contener la ruta de la imagen desde Supabase
   final String description;
   final String logoImagePath;
   final String logoText;
 
   ImageItem({
-    required this.imagePath,
+    required this.imageData, // Cambiado de File a String
     required this.description,
     required this.logoImagePath,
     required this.logoText,
   });
-
-  // Nuevo constructor para cargar desde JSON
-  ImageItem.fromJson(Map<String, dynamic> json)
-      : imagePath = json['imagePath'],
-        description = json['description'],
-        logoImagePath = json['logoImagePath'],
-        logoText = json['logoText'];
-
-  // Convierte el widget a JSON
-  Map<String, dynamic> toJson() => {
-        'imagePath': imagePath,
-        'description': description,
-        'logoImagePath': logoImagePath,
-        'logoText': logoText,
-      };
 
   @override
   _ImageItemState createState() => _ImageItemState();
@@ -190,14 +161,16 @@ class ImageItem extends StatefulWidget {
 
 class _ImageItemState extends State<ImageItem> {
   int likeCount = 0;
-  bool isVoted = false;
+  bool isVoted = false; // Variable para indicar si se ha dado like
 
+  // Método para cambiar el estado de "like"
   void toggleVote() {
     setState(() {
       isVoted = !isVoted;
     });
   }
 
+  // Método para manejar el voto (like)
   void handleVote() {
     setState(() {
       if (isVoted) {
@@ -205,12 +178,16 @@ class _ImageItemState extends State<ImageItem> {
       } else {
         likeCount++;
       }
-      toggleVote();
     });
+    toggleVote(); // Mover esta llamada fuera de setState()
   }
 
   @override
   Widget build(BuildContext context) {
+    // Decodificar los datos base64
+    Uint8List imageDataBytes = base64Decode(widget.imageData);
+
+    // Retornar el widget ImageItem con la imagen cargada desde datos decodificados
     return Align(
       alignment: Alignment.centerLeft,
       child: Column(
@@ -227,30 +204,33 @@ class _ImageItemState extends State<ImageItem> {
               Text(widget.logoText, style: TextStyle(color: Colors.white)),
             ],
           ),
-          Image.file(File(widget.imagePath)),
+          // Mostrar la imagen desde los datos decodificados
+          Image.memory(
+            imageDataBytes,
+            width: MediaQuery.of(context).size.width, // Ancho máximo
+            fit: BoxFit.cover, // Ajuste de la imagen
+          ),
           SizedBox(height: 8), // Margen por encima de los botones
           Container(
-            margin: EdgeInsets.only(
-                left: 9.0), // Ajusta el valor según el margen deseado
+            margin: EdgeInsets.only(left: 9.0),
             child: Row(
               children: [
-                IconButton(
-                  onPressed: () {
-                    handleVote();
-                  },
-                  icon: Icon(
-                    isVoted ? Icons.favorite : Icons.favorite_border,
-                    color: isVoted ? Colors.white : Colors.white,
+                SizedBox(
+                  width: 40, // Ancho específico para el botón
+                  child: IconButton(
+                    onPressed: handleVote,
+                    icon: Icon(
+                      isVoted ? Icons.favorite : Icons.favorite_border,
+                      color: isVoted ? Colors.white : Colors.white,
+                    ),
                   ),
                 ),
-                SizedBox(
-                    width: 0), // Espacio entre el icono y el campo de texto
-                Text('${likeCount}',
-                    style: TextStyle(fontSize: 16, color: Colors.white)),
+                SizedBox(width: 0), // Espacio entre el icono y el campo de texto
+                Text('${likeCount}', style: TextStyle(fontSize: 16, color: Colors.white)),
                 SizedBox(width: 16), // Espacio entre los botones
                 IconButton(
                   onPressed: () {
-                    // Agrega la lógica para el botón "Share"
+                    // Lógica para el botón de compartir
                   },
                   icon: Icon(
                     isVoted ? Icons.share : Icons.share,
@@ -262,11 +242,8 @@ class _ImageItemState extends State<ImageItem> {
           ),
           SizedBox(height: 0), // Margen por debajo de los botones
           Padding(
-            padding: const EdgeInsets.only(
-                left: 18.0,
-                bottom: 40.0), // Ajusta los valores según el margen deseado
-            child:
-                Text(widget.description, style: TextStyle(color: Colors.white)),
+            padding: const EdgeInsets.only(left: 18.0, bottom: 40.0),
+            child: Text(widget.description, style: TextStyle(color: Colors.white)),
           ),
         ],
       ),
